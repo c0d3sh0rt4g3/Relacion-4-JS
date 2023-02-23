786d77d2302967beba32d0733bf54d6ccddb4401
0ut0f4mm0 <eriking2004@gmail.com>
2023-02-23 14:18:05 +0100

    acabado el 5

--- js/05.js
+++ js/05.js
@@ -4,4 +4,4 @@ var dias = ["Lunes", "Martes","Miércoles", "Jueves", "Viernes", "Sábado", "Dom
 document.write("En una sola sentencia: " + dias.join(", ") + "<br><br>")
 document.write("Invertido: " + dias.reverse() + "<br><br>")
 document.write("Ordenado alfabeticamente: " + dias.sort() + "<br><br>")
-document.write("Posicion de Lunes: " + dias.indexOf("Lunes") + "<br><br>")
+document.write("Posicion de Lunes respecto al orden alfabético: " + dias.indexOf("Lunes") + "<br><br>")
